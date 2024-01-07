6483a5bd87ed8bc5e4d94c5d6cd3c0094e2376a8
Panth Patel <panthhp@outlook.com>
2024-01-07 17:55:09 +0000

    send friend request and notification

--- src/friend-request/friend-request.controller.ts
+++ src/friend-request/friend-request.controller.ts
@@ -1,9 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { firestore, sendNotification } from '../util/firebase';
+import { platforms } from 'src/util/constants';
 
 @Controller('friend-request')
 export class FriendRequestController {
   @Get('send')
-  sendFriendRequest(): string {
-    return 'Send';
+  async sendFriendRequest(
+    @Query('from') from: string,
+    @Query('to') to: string,
+  ): Promise<string> {
+    const userFrom = await firestore.doc(`users/${from}`).get();
+    const userTo = await firestore.doc(`users/${to}`).get();
+
+    if (!userFrom.exists) return 'User from does not exist';
+    if (!userTo.exists) return 'User to does not exist';
+
+    if (
+      !(
+        await firestore
+          .collection(`friends`)
+          .where('from', '==', userFrom.ref)
+          .where('to', '==', userTo.ref)
+          .get()
+      ).empty
+    ) {
+      return 'Friend request already exists';
+    }
+
+    await firestore.collection('friends').add({
+      from: userFrom.ref,
+      to: userTo.ref,
+      users: [userFrom.ref, userTo.ref],
+      state: 'pending',
+    });
+
+    let tokens = [];
+
+    for (const platform in platforms) {
+      tokens = tokens.concat(userTo.data[`${platform}_tokens`]);
+    }
+
+    sendNotification({
+      tokens: tokens,
+      title: `${userFrom.data['displayName']} as sent you a friend request!`,
+      body: 'Click this notification to open the app',
+      data: {
+        type: 'friendRequest',
+        fromDisplayName: userFrom.data['displayName'],
+        fromPhotoUrl: userFrom.data['photoUrl'],
+        click_action: 'FLUTTER_NOTIFICATION_CLICK',
+      },
+      image: userFrom.data['photoUrl'],
+    });
   }
 }
--- src/util/firebase.ts
+++ src/util/firebase.ts
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin';
 import { ConfigModule } from '@nestjs/config';
+import { logo } from './constants';
 
 ConfigModule.forRoot();
 
@@ -14,3 +15,49 @@ const app = admin.initializeApp({
 export const fcm = admin.messaging(app);
 export const firestore = admin.firestore(app);
 export const auth = admin.auth(app);
+
+type Notification = {
+  title: string;
+  body: string;
+  data: { [key: string]: string };
+  image: string;
+  tokens: string[];
+};
+
+export async function sendNotification(notification: Notification) {
+  await fcm.sendEachForMulticast({
+    tokens: notification.tokens,
+    data: notification.data,
+    android: {
+      priority: 'high',
+      notification: {
+        title: notification.title,
+        body: notification.body,
+        channelId: 'max_importance_channel',
+        imageUrl: notification.image,
+        clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+        icon: '@drawable/ic_notification',
+      },
+      data: notification.data,
+    },
+    notification: {
+      title: notification.title,
+      body: notification.body,
+      imageUrl: notification.image,
+    },
+    webpush: {
+      data: notification.data,
+      fcmOptions: {
+        link: 'https://tetraconnect.web.app/',
+      },
+      notification: {
+        badge: logo,
+        icon: logo,
+        image: notification.image,
+        data: notification.data,
+        title: notification.title,
+        body: notification.body,
+      },
+    },
+  });
+}
