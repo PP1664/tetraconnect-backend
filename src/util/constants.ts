084bc8aad497e4cd0ad95656981fd80b2d26d43a
Panth Patel <panthhp@outlook.com>
2024-01-07 17:54:28 +0000

    constants

---
+++ src/util/constants.ts
@@ -0,0 +1,11 @@
+export const platforms = [
+  'android',
+  'ios',
+  'web',
+  'windows',
+  'linux',
+  'fuchsia',
+  'macos',
+];
+export const logo =
+  'https://firebasestorage.googleapis.com/v0/b/tetraconnect.appspot.com/o/app%2Flogo.png?alt=media&token=b4855885-c37b-4cee-a773-a847bc30dd73';
