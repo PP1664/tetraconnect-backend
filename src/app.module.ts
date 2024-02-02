3e858def10f3fbed5308846c0ae9ac2fff7bf48b
PP6464 <panthhp@outlook.com>
2024-02-02 15:35:36 +0000

    friend controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { FriendRequestController } from './friend-request/friend-request.controller';
+import { FriendController } from './friend/friend.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, FriendRequestController],
+  controllers: [AppController, FriendRequestController, FriendController],
 })
 export class AppModule {}
